2aad4ade94e00d10c6159902639fbf7f019c31f0
Oskaras <oskaraskanasevicius@outlook.com>
2017-12-11 00:40:56 +0200

    Styling

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,19 +3,27 @@ import Option from './Option';
 
 
 const Options = (props) => (
-      <div>
-          <button onClick={props.handleDeleteOptions}>Remove all options</button>
-          {props.options.length === 0 && <p>Please add an option!</p>}
-          {
-              props.options.map((option) => (
-                  <Option
-                      key={option}
-                      optionText={option}
-                      handleDeleteOption={props.handleDeleteOption}
-                  />
-              ))
-          }
-      </div>
-  );
+    <div>
+        <div className="widget-header">
+            <h3>Your Options</h3>
+            <button
+                className="button button--link"
+                onClick={props.handleDeleteOptions}>
+                Remove all options
+            </button>
+        </div>
+
+        {props.options.length === 0 && <p>Please add an option!</p>}
+        {
+            props.options.map((option) => (
+                <Option
+                    key={option}
+                    optionText={option}
+                    handleDeleteOption={props.handleDeleteOption}
+                />
+            ))
+        }
+    </div>
+);
 
 export default Options;
